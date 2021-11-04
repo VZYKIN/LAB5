4b7418485da4a1671765726d02d910d374e12a34
VZYKIN <92172085+VZYKIN@users.noreply.github.com>
2021-11-04 21:42:06 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        VZ=nums1+nums2
+        VZ.sort()
+        n=len(VZ)
+        if len(VZ)==0 or len(VZ)==1:
+            return VZ[0]
+        if n%2!=0:
+            s=(n/2)
+            return VZ[s]
+        else:
+            return (VZ[n/2]+VZ[(n/2)-1])/2.0
